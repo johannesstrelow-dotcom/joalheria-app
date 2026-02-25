e1d5da19aead99c6908e197ac1e812475b9bc76d
johannesstrelow-dotcom <joka.night@gmail.com>
2026-02-25 13:46:57 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-6">
+      <h1 className="text-3xl font-bold text-pink-600">
+        Joalheria App
+      </h1>
+      <p>Projeto Next.js funcionando no Vercel.</p>
+    </main>
+  );
+}
